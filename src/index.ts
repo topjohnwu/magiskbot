3adb2fea4f92b0a9a97a2e459d482c38e8bf8033
topjohnwu <topjohnwu@gmail.com>
2025-12-05 03:34:45 -0800

    Several improvements
    - Launch web server first
    - Stop running blockAllSpam and updateCountJson on server start

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { blockAllSpam } from './utils.js';
 import server from './webhook.js';
 import updateCountJson from './count.js';
 
 async function main() {
-  // Scan and block all spam every 8 hours
-  await blockAllSpam();
-  setInterval(blockAllSpam, 8 * 60 * 60 * 1000);
-
-  // Count downloads every 24 hours
-  await updateCountJson();
-  setInterval(updateCountJson, 24 * 60 * 60 * 1000);
-
   // Start webhook server
   try {
     await server.listen({ port: Number(process.env.PORT), host: '0.0.0.0' });
@@ -19,6 +10,9 @@ async function main() {
   } catch (err) {
     console.log(err);
   }
+
+  // Count downloads every 24 hours
+  setInterval(updateCountJson, 24 * 60 * 60 * 1000);
 }
 
 await main();
